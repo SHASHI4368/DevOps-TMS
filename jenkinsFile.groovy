52afd5971503ec494219a945a1a389c85de425c7
Shashika Gurunayake <shashikagurunayake3198@gmail.com>
2024-06-29 02:42:35 +0530

    check for docker

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -15,12 +15,17 @@ pipeline {
     }
 
     stages {
-        stage('Check Docker Availability') {
+        stage('Access the Deploy Server') {
             steps {
                 script {
                     sshagent([SSH_CREDENTIALS_ID]) {
                         sh """
                         ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_HOST} << 'EOF'
+                        if [ ! -d "~/${APP_NAME}" ]; then
+                            git clone ${REPO_URL} ~/${APP_NAME}
+                        fi
+                        cd ~/${APP_NAME}
+                        git pull origin ${BRANCH}
                         if ! command -v docker &> /dev/null; then
                             echo "Docker not found. Installing Docker..."
                             sudo apt-get update
@@ -37,24 +42,6 @@ pipeline {
                             sudo apt-get update
                             sudo apt-get install -y docker-ce docker-ce-cli containerd.io
                         fi
-                        EOF
-                        """
-                    }
-                }
-            }
-        }
-
-        stage('Access the Deploy Server') {
-            steps {
-                script {
-                    sshagent([SSH_CREDENTIALS_ID]) {
-                        sh """
-                        ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_HOST} << 'EOF'
-                        if [ ! -d "~/${APP_NAME}" ]; then
-                            git clone ${REPO_URL} ~/${APP_NAME}
-                        fi
-                        cd ~/${APP_NAME}
-                        git pull origin ${BRANCH}
                         if ! command -v docker-compose &> /dev/null; then
                             sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
                             sudo chmod +x /usr/local/bin/docker-compose
