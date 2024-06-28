e6dceba54632a302f95e9f6b10db1cd3a112cfc2
Shashika Gurunayake <shashikagurunayake3198@gmail.com>
2024-06-29 02:35:17 +0530

    check for docker

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -26,6 +26,21 @@ pipeline {
                         fi
                         cd ~/${APP_NAME}
                         git pull origin ${BRANCH}
+                        if ! command -v docker &> /dev/null; then
+                          echo "Docker not found. Installing Docker..."
+                          sudo apt-get update
+                          sudo apt-get install ca-certificates curl
+                          sudo install -m 0755 -d /etc/apt/keyrings
+                          sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                          sudo chmod a+r /etc/apt/keyrings/docker.asc
+                          echo \
+                            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                          sudo apt-get update
+
+                          sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+                        fi
                         if ! command -v docker-compose &> /dev/null; then
                           sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
                           sudo chmod +x /usr/local/bin/docker-compose
