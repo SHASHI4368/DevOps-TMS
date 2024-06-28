4928ed5a5aad5d57f22b1def328e5563ce200433
Shashika Gurunayake <shashikagurunayake3198@gmail.com>
2024-06-29 02:40:36 +0530

    check for docker

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -15,17 +15,12 @@ pipeline {
     }
 
     stages {
-        stage('Access the Deploy Server') {
+        stage('Check Docker Availability') {
             steps {
                 script {
                     sshagent([SSH_CREDENTIALS_ID]) {
                         sh """
                         ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_HOST} << 'EOF'
-                        if [ ! -d "${APP_NAME}" ]; then
-                            git clone ${REPO_URL} ${APP_NAME}
-                        fi
-                        cd ${APP_NAME}
-                        git pull origin ${BRANCH}
                         if ! command -v docker &> /dev/null; then
                             echo "Docker not found. Installing Docker..."
                             sudo apt-get update
@@ -42,9 +37,26 @@ pipeline {
                             sudo apt-get update
                             sudo apt-get install -y docker-ce docker-ce-cli containerd.io
                         fi
+                        EOF
+                        """
+                    }
+                }
+            }
+        }
+
+        stage('Access the Deploy Server') {
+            steps {
+                script {
+                    sshagent([SSH_CREDENTIALS_ID]) {
+                        sh """
+                        ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_HOST} << 'EOF'
+                        if [ ! -d "~/${APP_NAME}" ]; then
+                            git clone ${REPO_URL} ~/${APP_NAME}
+                        fi
+                        cd ~/${APP_NAME}
+                        git pull origin ${BRANCH}
                         if ! command -v docker-compose &> /dev/null; then
-                            echo "Docker Compose not found. Installing Docker Compose..."
-                            sudo curl -sSL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+                            sudo curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
                             sudo chmod +x /usr/local/bin/docker-compose
                         fi
                         sudo docker-compose down
